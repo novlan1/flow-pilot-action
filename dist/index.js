e600625e88b0ea3a8d03a325f46ad33c0f913890
novlan1 <1576271227@qq.com>
2025-11-25 11:04:30 +0800

    chore: update publish script

--- dist/index.js
+++ dist/index.js
@@ -59542,7 +59542,7 @@ function pull_request_target(token) {
                     (0, core_1.info)(`${release.name} is private package, skip publish`);
                     return;
                 }
-                const { stdout } = yield (0, exec_1.getExecOutput)('pnpm', ['--filter', release.name, 'run', 'publish']);
+                const { stdout } = yield (0, exec_1.getExecOutput)('pnpm', ['--filter', release.name, 'run', 'release']);
                 (0, core_1.info)(stdout);
             }));
         }
